6ebfacfd4a014e13f547f49e5da4d6fd3074852d
JefferyLai5354 <110510298+JefferyLai5354@users.noreply.github.com>
2022-08-17 14:39:56 +0800

    Add files via upload

---
+++ cover2.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+
+def sharpness(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    lap = cv2.Laplacian(img, cv2.CV_16S)
+    mean, stddev = cv2.meanStdDev(lap)
+    return stddev[0,0]
+
+def main():
+    #Video capturing starts
+    image = cv2.VideoCapture(0)
+    
+
+    while(True):
+        ret, frame = image.read()
+        
+        #Check sharpness
+        #cv2.putText(frame, str(sharpness(frame)), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0 ,255, 255), 1, cv2.LINE_AA) 
+        print(sharpness(frame))
+
+        if sharpness(frame) < 11:
+            cv2.putText(frame, "Covered!", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0 ,255, 255), 1, cv2.LINE_AA) 
+
+        cv2.imshow('Frame', frame)
+        #Close camera with "esc"        
+        k = cv2.waitKey(2) & 0xff 
+        if k == 27:
+            break
+
+    #releasing camera and closing all the windows
+    image.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
